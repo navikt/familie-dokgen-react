75f170eb70bbe75c0da64d2ecb4551ea9845acb2
Miriam Størseth Lillebo <miriamlillebo@Miriams-MacBook-Pro.local>
2019-06-25 14:28:49 +0200

    React setup

---
+++ dokumentgenerator/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hei venner, nå skal det lages dokumentgenerator! 
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
