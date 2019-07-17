3c60cbf759e03d9107333b8d87f836d742df3c63
Peter Rydberg <hmp.rydberg@gmail.com>
2019-07-17 13:36:55 +0200

    Update endpoints to Norwegian

--- dokumentgenerator/src/API/url.js
+++ dokumentgenerator/src/API/url.js
@@ -1,11 +1,11 @@
 export const BASE = '';//'http://localhost:8080';
 
 //// Templates ////
-export const GET_TEMPLATE = `${BASE}/template/`;
-export const GET_ALL_TEMPLATE_NAMES = `${BASE}/template/all/`;
-export const POST_TEMPLATE = `${BASE}/template/`;
-export const PUT_TEMPLATE = `${BASE}/template/`;
-export const DELETE_TEMPLATE = `${BASE}/template/`;
+export const GET_TEMPLATE = `${BASE}/mal/`;
+export const GET_ALL_TEMPLATE_NAMES = `${BASE}/mal/alle/`;
+export const POST_TEMPLATE = `${BASE}/mal/`;
+export const PUT_TEMPLATE = `${BASE}/mal/`;
+export const DELETE_TEMPLATE = `${BASE}/mal/`;
 
 //// Letter ////
-export const POST_LETTER = `${BASE}/letter/`;
+export const POST_LETTER = `${BASE}/brev/`;
