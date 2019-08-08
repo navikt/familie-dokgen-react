faaec173453969d1a36c220df98ef5699d89bb2e
Peter Rydberg <hmp.rydberg@gmail.com>
2019-08-08 15:48:24 +0200

    Removed warnings

--- dokumentgenerator/src/containers/PreviewContainer.js
+++ dokumentgenerator/src/containers/PreviewContainer.js
@@ -36,7 +36,7 @@ class PreviewContainer extends Component {
             if(this.props.previewFormat === "pdf") {
                 return (
                     <Knapp type="standard"
-                        style={{margin:"auto", margin:"1em auto 3em auto", display:"flex", justifyContent: "center"}}
+                        style={{margin:"1em auto 3em auto", display:"flex", justifyContent: "center"}}
                         onClick={() => this.props.downloadPdf(this.props.selectedTemplate, this.props.selectedTestData)}
                     >
                         Last ned PDF
